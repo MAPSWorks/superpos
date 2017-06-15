51ab0450b1763530b1d032683564081285ac8053
byldin <byldin@mail.ru>
2017-06-15 11:56:39 +0300

    Объединил несколько панелей в toolbox

--- widget.cpp
+++ widget.cpp
@@ -4,6 +4,7 @@
 #include "locator.h"
 
 #include <iostream>
+#include <QToolBox>
 
 using namespace std;
 
@@ -12,55 +13,39 @@ Widget::Widget():
 {
   ui->setupUi(this);
 
- /* locators.clear();
-
-  locators.push_back(Locator());
-  locators.back().init(QPointF(COORDS(34.0, 59.0, 44.07), COORDS(56.0,  8.0, 49.83)),
-            "/windows/Work/IANS/polinom/Эксперименты_10_6хРЛС/2_250316/RLS_1_fileRLS_FFT_001.b", 501); // 44 град
-*/
-
-/*
-  locators.push_back(Locator());
-  locators.back().init(QPointF(COORDS(34.0, 58.0, 50.20), COORDS(56.0,  8.0, 29.59)),
-            "/windows/Work/IANS/polinom/Эксперименты_10_6хРЛС/2_250316/RLS_4_fileRLS_FFT_001.b", 1115); // 98 град
-*/
-
-/*
-  targets.push_back(Target(new LinearMotionModel(QPointF(COORDS(34.0, 59.0, 39.304), COORDS(56.0,  8.0, 42.764)),
-                           0.03 * QPointF((COORDS(34.0, 58.0, 56.451) - COORDS(34.0, 59.0, 39.304)),
-                                          COORDS(56.0,  8.0, 32.691) - COORDS(56.0,  8.0, 42.764)))));
-*/
-
-
-//  points_vector.push_back(QPointF(COORDS(34.0, 59.0, 39.304), COORDS(56.0,  8.0, 42.764)));
-//  points_vector.push_back(QPointF(COORDS(34.0, 58.0, 56.451), COORDS(56.0,  8.0, 32.691)));
-//  points_vector.push_back(QPointF(COORDS(34.0, 58.0, 10.451), COORDS(56.0,  8.0, 32.691)));
-//  targets.push_back(Target(new PoligonalMotionModel(pv, 0.002)));
-
   mv = new Mapviewer(this);
   mv->setGeometry(10,10,650,650);
   mv->setView(locators_ctrl.getCenter());
   mv->updatePixmapAzim(DISCR_NUM*SCALE*METERS_IN_DISCR, 0);
 
   // Панель локаторов
-  locators_ctrl.initTabWidget(this, QRect(680, 20, 340, 260));
-
-  // Панель общих параметров
-  params.init(this, QRect(680, 450, 340, 200));
+  locators_ctrl.initTabWidget(this, QRect(0, 0, 340, 260));
 
   connect(ui->pbAddLoc, SIGNAL(released()), &locators_ctrl, SLOT(addLocator()));
   connect(ui->pbDelLoc, SIGNAL(released()), &locators_ctrl, SLOT(deleteLocators()));
   connect(ui->pbUpdate, SIGNAL(released()), &locators_ctrl, SLOT(updateTabWidget()));
   connect(ui->pbUpdate, SIGNAL(released()), this, SLOT(updateLocators()));
   connect(ui->pbReset,  SIGNAL(released()), this, SLOT(optimizeView()));
-
   connect(&locators_ctrl, SIGNAL(eventUpdate()), this, SLOT(updateLocators()));
 
-  connect(ui->pbStartImit, SIGNAL(released()), SLOT(startImit()));
-  connect(ui->pbStopImit,  SIGNAL(released()), SLOT(stopImit()));
+  // Панель общих параметров
+  params.init(this, QRect(0, 0, 340, 200));
+
+  // Управление целями
   connect(ui->pbBeginAddTarg, SIGNAL(released()), SLOT(beginAddTarget()));
   connect(ui->pbEndAddTarg,   SIGNAL(released()), SLOT(endAddTarget()));
   connect(ui->pbDeleteTargs,  SIGNAL(released()), SLOT(deleteTargets()));
+
+  // Управление имитацией
+  connect(ui->pbStartImit, SIGNAL(released()), SLOT(startImit()));
+  connect(ui->pbStopImit,  SIGNAL(released()), SLOT(stopImit()));
+
+  // Объединяем панели в ToolBox
+  QToolBox *tb = new QToolBox(this);
+  tb->addItem(&locators_ctrl.getTabWidget(), "Локаторы");
+  tb->addItem(&params, "Общие параметры");
+  tb->setGeometry(680, 20, 340, 350);
+
 
   connect(&timer, SIGNAL(timeout()), this, SLOT(updateTargets()));
 
